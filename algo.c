8387caaf54d753ffca49b4224d28f91f49a21c5a
Edie Zhou <edie198@gmail.com>
2020-04-26 15:27:44 -0500

    Added line index functions

--- algo.c
+++ algo.c
@@ -7,10 +7,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-const int ASCII_OFF = 32;
-
 int* horspool_match (char* pattern, char* text);
 int* create_shifts (char* pattern);
+char* get_line_start (char* text, int idx);
+char* get_line_end (char* text, int idx, int pattern_len);
 
 /**
  *  Driver function
@@ -22,6 +22,13 @@ int main(int argc, char* argv[])
   char* test_pattern = "test";
   int* occ = horspool_match(test_pattern, test_str);
   // printf("Occurences of %s: %d\n", test_pattern, occ);
+  
+  size_t occ_size = sizeof(occ) / sizeof(int);
+  for (int i = 0; i < occ_size; ++i) {
+    print("%d\n", occ[i]);
+  }
+
+  int pat_length = strlen(test_pattern);
   return 0;
 }
 
@@ -39,7 +46,7 @@ int main(int argc, char* argv[])
 int* horspool_match (char* pattern, char* text)
 {
   // Offset for first ASCII character
-
+  const int ASCII_OFF = 32;
 
   int* result;
   int idx = 0;
@@ -88,6 +95,9 @@ int* horspool_match (char* pattern, char* text)
  */ 
 int* create_shifts (char* pattern)
 {
+  // Offset for first ASCII character
+  const int ASCII_OFF = 32;
+
   // Printable ASCII chars are 32-126 inclusive
   const int TABLE_SIZ = 95;
   
@@ -95,13 +105,66 @@ int* create_shifts (char* pattern)
   int* shift_table = (int*) malloc (sizeof(int) * TABLE_SIZ);
   
   for(int i = 0; i < TABLE_SIZ; i++) {
+    // set all entries to longest shift (pattern length)
     shift_table[i] = length;
   }
   for(int j = 0; j < length - 1; j++) {
+    // set pattern characters to shortest shifts
     shift_table[pattern[j] - ASCII_OFF] = length - 1 - j;
   }
 
   return shift_table;
+}
+
+/**
+ *  Purpose:
+ *    Get text line start index from pattern index
+ *  
+ *  Args:
+ *    text       {char*}: Text c-string
+ *    idx          {int}: Pattern start index
+ *  
+ *  Returns:
+ *    {char*}: Line of text  
+ */ 
+char* get_line_start (char* text, int idx)
+{
+  const char NEW_LINE = '\n';
+
+  int start_idx = idx;
+
+  while (start_idx != 0 && text[start_idx] != NEW_LINE) {
+    --start_idx;
+  }
+
+  return start_idx;
+}
+
+/**
+ *  Purpose:
+ *    Get inclusive text line end index from pattern index
+ *  
+ *  Args:
+ *    text       {char*}: Text c-string
+ *    idx          {int}: Pattern start index
+ *    pattern_len  {int}: Optional param, pass if you know pattern length
+ *  
+ *  Returns:
+ *    {char*}: Line of text  
+ */ 
+char* get_line_end (char* text, int idx, int pattern_len)
+{
+  const char NULL_TERM = '\0';
+  const char NEW_LINE = '\n';
+
+  int end_idx = idx;
+
+  while (text[end_idx] != NULL_TERM && text[end_idx] != NEW_LINE) {
+    // Increment until new line or null terminator
+    ++end_idx;
+  }
+
+  return end_idx - 1;
 }
 
 
